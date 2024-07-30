b9c8eb6bdcb06893522d40c7db3bcec4c10ebf1c
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-30 15:53:37 -0300

    Add project files.

---
+++ ListaObjetos/Eletronicos.cs
@@ -0,0 +1,15 @@
+﻿namespace ListaObjetos;
+
+public class Eletronicos
+{
+
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public Data Data { get; set; }
+}
+
+public class Data
+{
+    public string Color { get; set; }
+    public string Capacity { get; set; }
+}
---
+++ ListaObjetos/Program.cs
@@ -0,0 +1,56 @@
+﻿using CsvHelper;
+using ListaObjetos;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace DeviceFilter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var apiUrl = "https://api.restful-api.dev/objects";
+            var dispositivos = await GetDevicesAsync(apiUrl);
+
+            var marca = "Apple";
+
+            var dispositivosMarca = FilterDispositivos(dispositivos, marca);
+
+            foreach (var dispositivo in dispositivosMarca)
+            {
+                Console.WriteLine(dispositivo.Id + dispositivo.Name + dispositivo.Data?.Color + dispositivo.Data?.Capacity);
+                Console.WriteLine("-------------------------");
+            }
+
+            SaveToCsv(dispositivosMarca, $"{marca}_eletronicos.csv");
+        }
+
+        static async Task<List<Eletronicos>> GetDevicesAsync(string url)
+        {
+            using HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(url);
+            return JsonConvert.DeserializeObject<List<Eletronicos>>(response);
+        }
+
+        static List<Eletronicos> FilterDispositivos(List<Eletronicos> dispositivos, string marca)
+        {
+            var dispositivosMarca = new List<Eletronicos>();
+
+            foreach (var dispositivo in dispositivos)
+            {
+                if (dispositivo.Name.Contains(marca))
+                {
+                    dispositivosMarca.Add(dispositivo);
+                }
+            }
+            return dispositivosMarca;
+        }
+
+        static void SaveToCsv(List<Eletronicos> dispositivos, string filePath)
+        {
+            using var writer = new StreamWriter(filePath);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csv.WriteRecords(dispositivos);
+        }
+    }
+}
